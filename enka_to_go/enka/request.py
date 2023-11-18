d70a1099f3b40f028e6dbb8f5964b91144d1890b
seriaati <seria.ati@gmail.com>
2023-11-18 17:27:49 +0800

    refactor: remove caching and turn fetch_showcase to a classmethod

--- enka_to_go/enka/request.py
+++ enka_to_go/enka/request.py
@@ -1,24 +1,17 @@
 from typing import Any, Dict
 
 import aiohttp
-import cachetools
 
 from .exceptions import raise_for_retcode
 from .models.response import ShowcaseResponse
 
+BASE_URL = "https://enka.network/api/uid/{uid}/"
+
 
 class EnkaNetworkAPI:
-    def __init__(self):
-        self.base_url = "https://enka.network/api/uid/{uid}/"
-        self._cache: cachetools.TTLCache[str, Dict[str, Any]] = cachetools.TTLCache(
-            maxsize=100, ttl=60
-        )
-
-    async def fetch_showcase(self, uid: str) -> ShowcaseResponse:
-        if cached := self._cache.get(uid):
-            return ShowcaseResponse(**cached)
-
-        url = self.base_url.format(uid=uid)
+    @classmethod
+    async def fetch_showcase(cls, uid: str) -> ShowcaseResponse:
+        url = BASE_URL.format(uid=uid)
         async with aiohttp.ClientSession(
             headers={"User-Agent": "Enka to GO"}
         ) as session:
@@ -27,5 +20,4 @@ class EnkaNetworkAPI:
                     raise_for_retcode(resp.status)
 
                 data: Dict[str, Any] = await resp.json()
-                self._cache[uid] = data
                 return ShowcaseResponse(**data)
