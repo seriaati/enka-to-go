e7f8567225c3a02ae10f2c9131557b88c650d423
seria <seria.ati@gmail.com>
2025-10-24 11:52:21 +0900

    fix: Fix updating HSR assets too

--- update_data.py
+++ update_data.py
@@ -6,8 +6,6 @@ import enka
 async def update_data() -> None:
     async with enka.GenshinClient() as api:
         await api.update_assets()
-    async with enka.HSRClient() as api:
-        await api.update_assets()
 
 
 asyncio.run(update_data())
